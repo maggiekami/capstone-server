b06a37d754280ed07c8c99dd42b6c549ec6ed6f8
maggie kaminska <marg.adm.speak@gmail.com>
2023-03-30 11:14:37 +0100

    set up knex

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 
 const app = express();
+app.use(express.json());
+
+// app.get("/", async (_req, res) => {
+//   res.json({ message: "hello" });
+// });
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
