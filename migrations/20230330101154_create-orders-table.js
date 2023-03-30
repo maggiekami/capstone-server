0219e9b65cee17f059de3f1bf5700d3ed4bec13f
maggie kaminska <marg.adm.speak@gmail.com>
2023-03-30 14:30:53 +0100

    add tables to datagrip

--- migrations/20230330101154_create-orders-table.js
+++ migrations/20230330101154_create-orders-table.js
@@ -1,15 +1,55 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.up = function(knex) {
-  
+exports.up = function (knex) {
+  return knex.schema
+    .createTable("user", (table) => {
+      table.increments("id").primary();
+      table.string("email").notNullable().unique();
+      table.string("fName").notNullable();
+      table.string("lName").notNullable();
+    })
+    .createTable("product", (table) => {
+      table.increments("id").primary();
+      table.string("name").notNullable();
+      table.float("price").notNullable();
+      table.string("image").notNullable();
+      table.text("description").notNullable();
+    })
+    .createTable("order", (table) => {
+      table.increments("id").primary();
+      table
+        .integer("user_id")
+        .unsigned()
+        .references("id")
+        .inTable("user")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table.string("address").notNullable();
+      table.string("fName").notNullable();
+      table.string("lName").notNullable();
+      table.decimal("total", 8, 2).notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable("product_order", (table) => {
+      table
+        .integer("product_id")
+        .unsigned()
+        .references("id")
+        .inTable("product")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+      table
+        .integer("order_id")
+        .unsigned()
+        .references("id")
+        .inTable("order")
+        .onUpdate("CASCADE")
+        .onDelete("CASCADE");
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
-exports.down = function(knex) {
-  
+exports.down = function (knex) {
+  return knex.schema
+    .dropTable("product_order")
+    .dropTable("order")
+    .dropTable("product")
+    .dropTable("user");
 };
