ce952122488390199fd18c10a890b397f87fb64c
maggie kaminska <marg.adm.speak@gmail.com>
2023-04-04 10:15:58 +0100

    add test data

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -14,4 +14,24 @@ router.get("/", async (_req, res) => {
   }
 });
 
+router.get("/:id", async (req, res) => {
+  const productId = req.params.id;
+  try {
+    const data = await knex("product").where({ id: productId });
+
+    if (!data.length) {
+      return res.status(404).json({
+        error: true,
+        message: `Could not find product with ID: ${productId}`,
+      });
+    }
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({
+      error: true,
+      message: `Could not fetch product for ${productId}`,
+    });
+  }
+});
+
 module.exports = router;
