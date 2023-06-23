e41da7828524fe2fe6db140b74d8e9c74a426bc7
maggie kaminska <marg.adm.speak@gmail.com>
2023-06-23 14:57:00 +0100

    final

--- server.js
+++ server.js
@@ -13,7 +13,8 @@ const userRoutes = require("./routes/userRoutes");
 const orderRoutes = require("./routes/orderRoutes");
 const checkoutRoutes = require("./routes/checkoutRoutes");
 
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+// app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors());
 
 app.use(express.json());
 app.use(express.static("public"));
