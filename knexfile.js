d9862e00da00ca8f30106d084a65f1d5ff2ae365
maggie kaminska <marg.adm.speak@gmail.com>
2023-06-23 14:28:21 +0100

    final

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,8 @@ require("dotenv").config({ path: path.resolve(__dirname, "/knexfile") });
 module.exports = {
   client: "mysql2",
   connection: {
-    host: "eu-cdbr-west-(03).cleardb.net",
+    // host: "eu-cdbr-west-(03).cleardb.net",
+    host: "eu-cdbr-west-03.cleardb.net",
     user: "b0cfe9a6668d05",
     password: "08ea0093",
     database: "heroku_65b667fb80c64e1",
@@ -13,4 +14,5 @@ module.exports = {
   },
 };
 
+// mysql://b0cfe9a6668d05:08ea0093@eu-cdbr-west-03.cleardb.net/heroku_65b667fb80c64e1?reconnect=true
 // mysql://b0cfe9a6668d05:08ea0093@eu-cdbr-west-03.cleardb.net/heroku_65b667fb80c64e1?reconnect=true
